84dbeb2d7801e8bd51a37d322a6075291c1b1a4a
Flik Jeong <jsh852@gmail.com>
2025-06-01 13:00:11 +0900

    PR3: Organisms 컴포넌트 개발 완료

---
+++ src/components/organisms/ChatWindow.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ChatBubble from "../molecules/ChatBubble";
+
+const ChatWindow = ({ messages }) => {
+  return (
+    <div
+      style={{
+        maxWidth: "400px",
+        margin: "0 auto",
+        padding: "10px",
+        border: "1px solid #ccc",
+        borderRadius: "5px",
+      }}
+    >
+      {messages.map((msg, index) => (
+        <ChatBubble
+          key={index}
+          avatarSrc={msg.avatarSrc}
+          username={msg.username}
+          message={msg.message}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ChatWindow;
--- src/index.js
+++ src/index.js
@@ -4,15 +4,25 @@ import Avatar from "./components/atoms/Avatar";
 import Username from "./components/atoms/Username";
 import MessageText from "./components/atoms/MessageText";
 import ChatBubble from "./components/molecules/ChatBubble";
+import ChatWindow from "./components/organisms/ChatWindow";
 
 const App = () => {
+  const messages = [
+    {
+      avatarSrc: "https://via.placeholder.com/40",
+      username: "John Doe",
+      message: "Hello, this is a test message!",
+    },
+    {
+      avatarSrc: "https://via.placeholder.com/40",
+      username: "Jane Smith",
+      message: "Hi John, nice to meet you!",
+    },
+  ];
+
   return (
     <div>
-      <ChatBubble
-        avatarSrc="https://via.placeholder.com/40"
-        username="John Doe"
-        message="Hello, this is a test message!"
-      />
+      <ChatWindow messages={messages} />
     </div>
   );
 };
